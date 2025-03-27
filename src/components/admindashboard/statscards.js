9ac32c7e98d6641c7bf3f28c1cca5d5b5cef1295
prashu-tech <prashantparsane894@gmail.com>
2025-03-27 17:04:17 +0530

    responsive nav and sidebar

--- src/components/admindashboard/statscards.js
+++ src/components/admindashboard/statscards.js
@@ -8,30 +8,30 @@ const StatsCards = () => {
     {
       id: 1,
       title: "Student Count",
-      image: "/dashboardimg1.svg", // Update with actual image URL
+      image: "/dashboardimg1.svg",
     },
     {
       id: 2,
       title: "Practice Test Given",
-      image: "/dashboardimg2.svg", // Update with actual image URL
+      image: "/dashboardimg2.svg",
     },
     {
       id: 3,
       title: "Batch Count",
-      image: "/dashboardimg3.svg", // Update with actual image URL
+      image: "/dashboardimg3.svg",
     },
   ];
 
   return (
-    <div className="grid grid-cols-1 md:grid-cols-3 gap-4 pt-8 pr-70 pl-70 drop-shadow-2xl">
+    <div className="grid grid-cols-3 sm:grid-cols-3 md:grid-cols-3 gap-4 pt-8 px-4 md:px-60 drop-shadow-2xl sm:p-5 md:pt-16 ">
       {stats.map((item) => (
         <div
           key={item.id}
-          className="bg-white rounded-lg p-4 border border-gray-200"
+          className="bg-white rounded-xl p-3 md:p-4 border border-gray-200 shadow-sm"
         >
           {/* Title */}
-          <div className="flex items-center gap-2 text-gray-700 font-semibold">
-            <FaRegImage className="text-lg" />
+          <div className="flex items-center gap-2 text-gray-700 font-semibold text-[5px] md:text-base">
+            <FaRegImage className="text-base md:text-lg" />
             <h2>{item.title}</h2>
           </div>
 
@@ -40,7 +40,7 @@ const StatsCards = () => {
             <img
               src={item.image}
               alt={item.title}
-              className="w-full h-25 object-cover rounded-lg" 
+              className="w-full h-auto max-h-40 md:max-h-52 object-contain rounded-lg"
             />
           </div>
         </div>
