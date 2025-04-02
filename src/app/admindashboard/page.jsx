e6bc994be5901469b6c5b6faa1b4cd8886929770
prashu-tech <prashantparsane894@gmail.com>
2025-04-02 16:17:09 +0530

    updated nav and sidebar

--- src/app/admindashboard/page.jsx
+++ src/app/admindashboard/page.jsx
@@ -3,15 +3,18 @@
 import React, { useState } from "react";
 
 // Import your nav components
-import Nav, { MobileNavbar, MobilebottomNavbar } from "@/components/nav/nav";
-import Sidebar from "@/components/sidebar/sidebar";
-import ModeSwitcher from "@/components/admindashboard/modeswitcher";
-import StatsCards from "@/components/admindashboard/statscards";
-import TopPerformersTable from "@/components/admindashboard/TopPerformersTable";
-import SpotlightOnImprovementTable from "@/components/admindashboard/spotlightonImprovementtable";
-import StudentLoginStatus from "@/components/admindashboard/studentLoginStatus";
-import TestResultDownload from "@/components/admindashboard/testresultdownload";
-import LoginAttendance from "@/components/admindashboard/loginattendance";
+
+import ModeSwitcher from "@/components/desktopadmindashboard/modeswitcher";
+import StatsCards from "@/components/desktopadmindashboard/statscards";
+import TopPerformersTable from "@/components/desktopadmindashboard/TopPerformersTable";
+import SpotlightOnImprovementTable from "@/components/desktopadmindashboard/spotlightonImprovementtable";
+import StudentLoginStatus from "@/components/desktopadmindashboard/studentLoginStatus";
+import TestResultDownload from "@/components/desktopadmindashboard/testresultdownload";
+import LoginAttendance from "@/components/desktopadmindashboard/loginattendance";
+import MobileNavbar from "@/components/mobilenav/mobilenav";
+import MobilebottomNavbar from "@/components/mobilenav/MobileBottomNavbar";
+import Sidebar from "@/components/desktopsidebar/sidebar";
+import DesktopNavbar from "@/components/desktopnav/nav";
 
 // Import the rest of your components
 
@@ -22,23 +25,19 @@ export default function Page() {
 
   return (
     <div className="min-h-screen md:flex bg-white">
-      {/* Mobile Navs */}
-      <div className="md:hidden block">
-        <MobileNavbar />
-      </div>
-      <div className="md:hidden block">
-        <MobilebottomNavbar />
-      </div>
 
-      {/* Sidebar Section (visible on md+) */}
+      {/* Desktop Sidebar Section (visible on md+) */}
       <div className="md:w-1/6 bg-[#007AFF]">
         <Sidebar />
       </div>
 
-      {/* Main Content Section */}
+      {/* Desktop navbar Section */}
       <div className="w-full md:w-5/6 md:flex-1 h-screen bg-white">
         {/* Desktop Navbar (hidden on mobile) */}
-        <Nav />
+        <DesktopNavbar />
+
+        {/* Page Content */}
+        <main className="hidden md:block"> 
 
         {/* Mode Switcher */}
         <div className="flex justify-center mt-4">
@@ -51,10 +50,10 @@ export default function Page() {
         <SpotlightOnImprovementTable selectedMode={selectedMode} />
 
         <div className="flex justify-between gap-8 mt-4">
-          <div className="md:pl-44 md:pr-0">
+          <div className="md:ml-60 md:pr-0 md:mr-7">
             <StudentLoginStatus />
           </div>
-          <div className="md:pr-64 md:pl-0">
+          <div className="md:mr-94 md:pl-0">
             <TestResultDownload />
           </div>
         </div>
@@ -62,6 +61,23 @@ export default function Page() {
         <div className="flex justify-center mt-4">
           <LoginAttendance />
         </div>
+
+        </main>
+
+
+        {/* Mobile View */}
+        <main className="block md:hidden">
+
+        {/* Mobile Navs */}
+        <MobileNavbar />
+
+        {/* Mobile Navs */}  
+        <MobilebottomNavbar />
+
+        {/* Page Content */}
+     
+
+        </main>
       </div>
     </div>
   );
